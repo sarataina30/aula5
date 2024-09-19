83457b90838e6eab6d01059e55112bef2810a210
sarataina30 <tainasara320@gmail.com>
2024-09-19 11:46:31 -0300

    exercicio da aula5

---
+++ index.js
@@ -0,0 +1,88 @@
+/*
+const primeiroValor=10
+const segundoValor=20
+const resultado= primeiroValor+segundoValor+4
+console.log(resultado)
+
+const primeiroValorr=3
+const segundoValorr=4 
+const resultadoo= primeiroValorr+segundoValorr
+console.log(resultadoo)
+
+const primeiroValoor=3
+const segundoValoor=5
+const resultadooo= primeiroValoor*segundoValoor/2
+console.log(resultadooo)
+
+const primeirovalor=5
+const segundovalor=4
+const resuultado=primeirovalor-segundovalor-1
+console.log(resuultado)
+
+const primeirovallor=234
+const segundovallor=5
+const restoDaDivisao=234%5
+console.log(restoDaDivisao)
+
+const novavariavel=1
+const a=2 
+console.log(novavariavel===a)
+console.log(novavariavel!==a)
+console.log(novavariavel>=a)
+console.log(novavariavel<=a)
+
+const A = true
+const b= false
+const c= true 
+console.log(A&&b)
+console.log(b&&c)
+console.log(a&&c)
+console.log(A&&b&&c)
+
+const Aa = true
+const Bb = false
+const Cc = true
+console.log(Aa||Bb)
+console.log(Bb||Cc)
+console.log(Aa||Cc)
+console.log(Aa||Bb||Cc)
+
+//let nome = prompt("Qual é o seu nome?")
+let born = Number (prompt("Qual é a sua data de nascimento?"))
+let ano = Number (prompt("Qual é o seu ano atual?"))
+
+//console.log ("seu nome é",nome)
+console.log("Voce tem",ano-born,"anos de idade")
+console.log ( "E maior de idade",ano-born >=18)
+console.log("Em 2050 voce tera",2050 -born,"anos de idade")
+*/
+
+let nome = prompt ('qual é o seu nome?')
+let favorita= prompt('qual é a sua cor favorita?')
+console.log("a cor favorita de "+nome+  "é" +favorita)
+console.log('a cor favorita de', nome ,'e´',favorita)
+
+const frase= "OlaAAa"
+const frasemaiuscula=frase.toUpperCase()
+console.log(frase.toUpperCase())
+
+const frasee="oooooi"
+const novafrasee=frasee.replaceAll("o","i")
+console.log(novafrasee)
+ 
+const raçacachorro=["pastor alemao","pug","shih tzu","golden","rottweiller"]
+const b= prompt ("inserir um numero de 0 a 4")
+console.log ( raçacachorro[b])
+
+const numeros= [1,2,3,4,5,6]
+console.log(numeros.length)
+numeros.push(7)
+console.log(numeros)
+numeros.splice(4,5)
+console.log(numeros,4,5)
+console.log(numeros.length)
+
+
+
+
+
